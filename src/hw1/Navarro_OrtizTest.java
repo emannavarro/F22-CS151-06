bb484033108198434701e9173dbbb47bb1c1323b
emannavarro <emannavarro@gmail.com>
2022-09-05 16:42:32 -0700

    Added new class to hw1

---
+++ src/hw1/Navarro_OrtizTest.java
@@ -0,0 +1,21 @@
+"""
+Emanuel Navarro-Ortiz
+016449108
+emanuel.navarro-ortiz@sjsu.edu
+due 9/7/22
+Working Status: Working and tested 9/5/22
+"""
+
+public class Navarro_OrtizTest{
+
+    public static void main(String[] args){
+
+	Student Emanuel = new Student("Emanuel", "Hello My name is Emanuel Navarro-Ortiz", 9108);
+
+    }
+
+    
+
+
+
+}
